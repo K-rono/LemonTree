66b924cbd6378220f55e55839ea4734da9f65d93
Qi Hong <zhenhong610@gmail.com>
2024-03-07 22:14:45 +0800

    Completed cycle logic

--- app/src/main/java/com/example/lemontree/MainActivity.kt
+++ app/src/main/java/com/example/lemontree/MainActivity.kt
@@ -1,28 +1,24 @@
 package com.example.lemontree
 
-import android.media.Image
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemontree.ui.theme.LemonTreeTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,31 +26,35 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LemonTreeTheme {
-
+                ResourceCycler()
             }
         }
     }
 }
-@Composable
-fun ResourceCycler(phase: Int) {
-    val phase by remember { mutableStateOf(1) }
-    val image: Int
-    val text : Int
 
+@Composable
+fun ResourceCycler(phase: Int = 1) {
+    var phase by remember { mutableStateOf(1) }
+    val image: Int
+    val text: Int
 
     when (phase) {
+
         1 -> {
             image = R.drawable.lemon_tree
             text = R.string.step1
         }
+
         2 -> {
             image = R.drawable.lemon_squeeze
             text = R.string.step2
         }
+
         3 -> {
             image = R.drawable.lemon_drink
             text = R.string.step3
         }
+
         else -> {
             image = R.drawable.lemon_restart
             text = R.string.step4
@@ -66,12 +66,25 @@ fun ResourceCycler(phase: Int) {
             .fillMaxSize()
             .wrapContentHeight(Alignment.CenterVertically),
         image,
-        text
+        text,
+        onClickHandler = {
+            if (phase < 4) {
+                phase++
+            } else {
+                phase = 1
+            }
+            Log.i("Phase", phase.toString()) //To print it out in Logcat
+        }
     )
 }
 
 @Composable
-fun ShowLemonTree(modifier: Modifier = Modifier, image: Int, text: Int) {
+fun ShowLemonTree(
+    modifier: Modifier = Modifier,
+    image: Int,
+    text: Int,
+    onClickHandler: () -> Unit
+) {
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -79,7 +92,8 @@ fun ShowLemonTree(modifier: Modifier = Modifier, image: Int, text: Int) {
         Image(
             painter = painterResource(id = image),
             contentDescription = "",
-            modifier = Modifier.clickable {})
+            modifier = Modifier.clickable(onClick = onClickHandler)
+        )
         Text(stringResource(id = text))
     }
 }
